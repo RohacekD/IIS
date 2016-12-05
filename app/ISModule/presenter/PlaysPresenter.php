3235fce450b73b5ab06c9197f86c084c403f2c3d
Dominik Roháček <RohacekD@gmail.com>
2016-12-05 21:02:54 +0100

    Plays list

--- app/ISModule/presenter/PlaysPresenter.php
+++ app/ISModule/presenter/PlaysPresenter.php
@@ -8,10 +8,10 @@
 
 namespace App\ISModule\Presenters;
 
+use Ublaboo\DataGrid\DataGrid;
 use App\ISModule\Forms;
 use App\ISModule\Model;
 use Nette;
-use Tracy\Debugger;
 
 
 class PlaysPresenter extends SecuredPresenter {
@@ -22,6 +22,10 @@ class PlaysPresenter extends SecuredPresenter {
 	 * @var Model\Play
 	 */
 	private $play;
+	/**
+	 * @var @persistent Nette\Database\Table\Selection
+	 */
+	private $gridDataSource;
 
 	public function actionAdd() {
 		$this->play = new Model\Play;
@@ -29,6 +33,59 @@ class PlaysPresenter extends SecuredPresenter {
 
 	public function actionEdit( $id ) {
 		$this->play = $this->getEm()->getRepository( Model\Play::class )->findOneById( $id );
+	}
+
+
+	public function actionList() {
+		$this->gridDataSource = $this->getEm()->createQueryBuilder()
+		                             ->select( 'play' )
+		                             ->from( Model\Play::class, 'play' );
+	}
+
+	public function createComponentListGrid( $name ) {
+		$grid = $this->makeGrid( $name );
+		$grid->setTemplateFile( __DIR__ . '/templates/Users/my-grid-detail.latte' );
+
+		return $grid;
+	}
+
+	/**
+	 * @param $name
+	 *
+	 * @return DataGrid
+	 */
+	private function makeGrid( $name ) {
+		$grid = new DataGrid( $this, $name );
+
+		$source = $this->gridDataSource;
+		$grid->setPrimaryKey( "id" );
+		$grid->setTranslator( $this->translator );
+		$grid->setDataSource( $source );
+
+		$presenter = $this;
+
+		$grid->addColumnText( 'name', 'Název' );
+		$grid->addColumnText( 'author', 'Autor' );
+		$grid->addColumnDateTime( 'timeNeeded', 'Čas k nastudování' );
+		$grid->addAction( "delete", "", "delete!" )
+		     ->setIcon( 'trash' )
+		     ->setTitle( 'Delete' )
+		     ->setClass( 'btn btn-xs btn-danger ajax' )
+		     ->setConfirm( 'Do you really want to delete role \'%s\'?', 'name' );
+		$grid->addActionCallback( "edit", "" )
+		     ->setIcon( 'pencil' )
+		     ->setTitle( 'Upravit' )
+		     ->setClass( 'btn btn-xs btn-warning ajax' )
+			->onClick[] = function ( $item_id ) use ( $presenter ) {
+			$presenter->redirect( "Plays:edit", [ $item_id ] );
+		};;
+
+		return $grid;
+	}
+
+	public function handleDelete( $id ) {
+		//Debugger::dump( $id );
+		//todo
 	}
 
 	protected function createComponentAddPlay() {
@@ -46,5 +103,5 @@ class PlaysPresenter extends SecuredPresenter {
 			$this->redirect( 'Plays:edit', [ $play->getId() ] );
 
 		}, $this->play );
-    }
+	}
 }
